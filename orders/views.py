9268af977a46857a5368c78a11c7352d97e96d6c
Breanne Beutler <breannebeutler@unomaha.edu>
2021-04-27 16:42:04 -0500

    import orders directory

---
+++ orders/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import OrderItem
+from .forms import OrderCreateForm
+from cart.cart import Cart
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+
+            cart.clear()
+        return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'form': form})
